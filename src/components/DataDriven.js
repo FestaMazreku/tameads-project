1bcc716aeb09d2fb9439d0288b6c73b64d9b2d58
Festa <festamazreku@gmail.com>
2024-02-15 15:43:19 +0100

    Worked on navigation and first part

---
+++ src/components/DataDriven.js
@@ -0,0 +1,7 @@
+import "../stylesheets/Home.css";
+
+function Data() {
+  return <div className="Data"></div>;
+}
+
+export default Data;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import Navbar from "./Navigationbar";
+import React from "react";
+import "../stylesheets/Home.css";
 
 function Home() {
   return (
     <div className="Home">
       <Navbar />
+      <img src="images/Dashboard2x.png" alt="Dashboard" className="dashboard" />
+      <br />
+      <img src="images/Group 10.png" alt="Logos" className="logos" />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
