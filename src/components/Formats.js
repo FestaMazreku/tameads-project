1442e6e467e4f106239a4ae00d0c0ff993218dc4
Festa <festamazreku@gmail.com>
2024-03-11 22:16:47 +0100

    Working with other tasks

--- src/components/Formats.js
+++ src/components/Formats.js
@@ -1,3 +1,5 @@
+// Update the Formats.js file
+
 import React from "react";
 import "../stylesheets/Formats.css";
 import { RiLoginCircleLine } from "react-icons/ri";
@@ -5,15 +7,14 @@ import { RiLoginCircleLine } from "react-icons/ri";
 function Formats() {
   return (
     <>
-      <center>
+      <div className="center">
         <h1> TameAds Formats </h1>
         <p>
-          {" "}
           Both publishers and brands will be happy to know that we cater to
-          different formats. We created a variety of formats that can be <br />{" "}
-          adjusted to the publisher's design themes and website behavior.{" "}
+          different formats. We created a variety of formats that can be
+          adjusted to the publisher's design themes and website behavior.
         </p>
-      </center>
+      </div>
 
       <br />
       <img
@@ -25,26 +26,56 @@ function Formats() {
       <h2> Latest News </h2>
 
       <p>
-        {" "}
         Stay ahead of industry trends with our ad tech blog. <br /> Read our
-        native advertising tips & tricks.{" "}
+        native advertising tips & tricks.
       </p>
 
       <p className="viewall">
         View all
         <span>
-          {" "}
-          <RiLoginCircleLine className="icon" size={35} />{" "}
+          <RiLoginCircleLine className="icon" size={35} />
         </span>
       </p>
-      <center>
+
+      <div className="center">
         <img
           src="images/Leadership.png"
           alt="CrossScreen"
           className="leadership"
         />
-      </center>
-      <br />
+
+        <br />
+        <br />
+        <br />
+        <div className="content light-gray-bg publishers-container">
+          <div className="image-container">
+            <img
+              src="images/Advertisers.png"
+              alt="audience"
+              className="advertisers"
+            />
+          </div>
+
+          <div className="text-container">
+            <b>
+              <h2 className="publishers"> Publishers </h2>
+            </b>
+
+            <p className="text2">
+              <h4> Join The Global Media Platform </h4>
+              Unlock incremental revenue from <br /> premium advertisers through
+              ad experiences <br /> that respect your users.
+            </p>
+
+            <p className="joinnow">
+              Join now
+              <span>
+                <RiLoginCircleLine className="icon" size={35} />
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Solutions.js
+++ src/components/Solutions.js
@@ -77,6 +77,7 @@ function Solutions() {
           </div>
         </div>
       </div>
+
     </>
   );
 }
