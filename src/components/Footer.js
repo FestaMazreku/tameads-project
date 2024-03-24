f8b2e80f459bd542b9fac6dcd22ea03806227267
Festa <festamazreku@gmail.com>
2024-03-24 16:35:00 +0100

    Working on the other tasks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,136 +1,79 @@
 import React from "react";
 import "../stylesheets/Footer.css";
 
-function Footer() {
+const Footer = () => {
   return (
     <footer className="footer">
-      <div className="tameads_footer section-padding">
-        <div className="footer-links">
-          <img
-            src="/images/TameAdslogo.png"
-            alt="TameAds Logo"
-            className="logo"
-          />
-          <p className="text-footer">
-            {" "}
-            Join the largest ad network in the world.{" "}
+      <div className="footer-container">
+        <div className="footer-column">
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
+            urna eget nunc varius lacinia.
           </p>
-          <br />
-          <div className="footer-links-div">
-            <h4> TameAds </h4>
-
-            <p> TameAds </p>
-
-            <p> TameAds </p>
-
-            <p> TameAds </p>
-
-            <p> TameAds </p>
-
-            <p> TameAds </p>
-
-            <p> TameAds </p>
-          </div>
-
-          <div className="footer-sublinks">
-            <h4> Sublinks </h4>
-
-            <p> Advertisers </p>
-
-            <p> Ad Formats </p>
-
-            <p> Agencies </p>
-
-            <p> Download </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-          </div>
-
-          <div className="footer-sublinks">
-            <h4> Sublinks </h4>
-
-            <p> Advertisers </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-          </div>
-
-          <div className="footer-sublinks">
-            <h4> Sublinks </h4>
-
-            <p> Advertisers </p>
-
-            <p> Ad Formats </p>
-
-            <p> Agencies </p>
-
-            <p> Download </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-          </div>
-
-          <div className="footer-sublinks">
-            <h4> Sublinks </h4>
-
-            <p> Advertisers </p>
-
-            <p> Ad Formats </p>
-
-            <p> Agencies </p>
-
-            <p> Download </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-          </div>
-
-          <div className="footer-sublinks">
-            <h4> Sublinks </h4>
-
-            <p> Advertisers </p>
-
-            <p> Ad Formats </p>
-
-            <p> Agencies </p>
-
-            <p> Download </p>
-
-            <p> Affiliate </p>
-
-            <p> Affiliate </p>
-          </div>
-
-          <div className="footer-sublinks">
-            <div className="social-media">
-              <img
-                src="images/socialmedia.png"
-                alt="Socialmedia"
-                className="socialmedia"
-              />
-            </div>
-          </div>
-
-          <div className="footer-below">
-            <div className="footer-copyright">
-              <p> &copy; 2023 - TameAds - All Rights Reserved.</p>
-            </div>
-          </div>
         </div>
+        <div className="footer-column">
+          <h2>Quick Links</h2>
+          <ul>
+            <li>
+              <p> Home </p>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h2>Services</h2>
+          <ul>
+            <li>
+              <a href="#">Service 1</a>
+            </li>
+            <li>
+              <a href="#">Service 2</a>
+            </li>
+            <li>
+              <a href="#">Service 3</a>
+            </li>
+            <li>
+              <a href="#">Service 4</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h2>Follow Us</h2>
+          <ul>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Twitter</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>
+            </li>
+            <li>
+              <a href="#">LinkedIn</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h2>Contact Us</h2>
+          <p>Email: example@example.com</p>
+          <p>Phone: 123-456-7890</p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
